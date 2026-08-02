dd71f339cdee766e231e563812028fdc4cb3c174
eli-devbr <elivelton.faria@outlook.com>
2026-08-02 10:57:50 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-// Fatores médios (conjuntos por m²)
-// Considera assentamento comum com pequena margem de segurança.
+// Niveladores (bases) por m²
 const fatores = {
-  "20x20": 20,
-  "30x30": 11,
-  "33x33": 9,
-  "45x45": 6.5,
-  "50x50": 5.5,
-  "60x60": 4.2,
-  "60x90": 3.6,
-  "60x120": 3.5,
-  "80x80": 3.2,
-  "90x90": 2.8,
-  "100x100": 2.6,
-  "120x120": 2.2,
-  "120x180": 1.8
+  "20x20": 80,
+  "30x30": 44,
+  "33x33": 36,
+  "45x45": 26,
+  "50x50": 22,
+  "60x60": 17,
+  "60x90": 14,
+  "60x120": 14,
+  "80x80": 13,
+  "90x90": 11,
+  "100x100": 10,
+  "120x120": 9,
+  "120x180": 8
 };
 
 // Mostra/esconde o campo de acabamento conforme o tipo
